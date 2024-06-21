b51d9b4eead692a8e351ce7b728c6a54a23d3ac5
Siddhartha Chakrabarty <csiddhartha2004@gmail.com>
2024-06-21 23:47:06 +0530

    commit-2

--- seeds/index.js
+++ seeds/index.js
@@ -18,10 +18,14 @@ const seedDB = async () => {
     await Campground.deleteMany({})
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000)
+        const price = Math.floor(Math.random() * 20) + 10
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
 
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: "https://source.unsplash.com/random/?camp",
+            price,
+            description: " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima est dolor praesentium architecto. Quod molestias,dolores repellendus harum et nostrum odit consequatur deleniti, veniam dolorem corporis deserunt animi ipsavoluptatum."
         })
         await camp.save()
     }
